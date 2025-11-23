5c928b7ef7183d1034e381edd6d9eeae93b9dbaf
AlaaElsayed202 <lolaa732004@gmail.com>
2025-11-23 09:57:13 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FanPage from './component/FanPage';
-import ConditionerPage from './component/conditionerPage/ConditionerPage';
-import MainHome from './component/MainHome/MainHome';
+import ConditionerPage from "./component/ConditionerPage";
+import MainHome from './component/MainHome';
 
 export default function App() {
   return (
